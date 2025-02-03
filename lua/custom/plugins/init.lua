26e526e5d7b684541c035d155d893073898a244a
FluffyChaos <23279594+fluffychaos@users.noreply.github.com>
2025-02-03 20:57:05 +0100

    Added CMake plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Plugins for using CMake. cmake-tools.nvim has optional integrations with both overseer and toggleterm
+  -- so I include them here as optional plugins.
+  {
+    'Civitasv/cmake-tools.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('cmake-tools').setup {}
+    end,
+  },
+  {
+    'stevearc/overseer.nvim',
+    optional = true,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    optional = true,
+  },
+}
