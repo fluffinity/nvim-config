88751894c8bca0f1b4a1134e68d9931e4cfc59fe
Fluffinity <23279594+fluffychaos@users.noreply.github.com>
2025-02-10 21:18:23 +0100

    Enable english spellchecking in markdown files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Enable spellchecking in markdown files
+vim.api.nvim_create_autocmd({ 'FileType', 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.md' },
+  command = 'setlocal spell spelllang=en_us',
+})
+
 return {
   -- Plugins for using CMake. cmake-tools.nvim has optional integrations with both overseer and toggleterm
   -- so I include them here as optional plugins.
